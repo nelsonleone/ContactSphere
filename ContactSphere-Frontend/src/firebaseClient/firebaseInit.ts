fda8e8365edaa8ecb29d7a1761d10f32c08df19f
Nelsonleone4945 <95982650+nelsonleone@users.noreply.github.com>
2023-06-06 07:17:39 +0100

    Made Firebase Auth Handler

---
+++ ContactSphere-Frontend/src/firebaseClient/firebaseInit.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+   apiKey: process.env.FIREBASE_API_KEY,
+   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+   appId: process.env.FIREBASE_APP_ID 
+}
+
+initializeApp(firebaseConfig)
+
+export const auth = getAut
