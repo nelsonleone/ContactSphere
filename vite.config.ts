1b7d2f3d08fc9ff407e7e2783cbb03dcfe406ef2
Nelson Onuegbu <leonenelson38@gmail.com>
2023-08-20 16:21:15 +0100

    rollback vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@mantine/hooks'],
-    },
-  }
+  plugins: [react()]
 })
