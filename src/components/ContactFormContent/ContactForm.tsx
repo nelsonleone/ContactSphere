70715b3cd1c6c5620295832c7ed3b37139a3a113
Nelson Onuegbu <leonenelson38@gmail.com>
2024-05-07 00:20:24 +0100

    update to contactForm

--- src/components/ContactFormContent/ContactForm.tsx
+++ src/components/ContactFormContent/ContactForm.tsx
@@ -131,11 +131,11 @@ function ContactForm({ action, contactId, defaultValue }: { defaultValue?:Contac
 
    useEffect(() => {
       // Disabled Save Button 
-      if(errors.firstName || errors.birthday || !phoneNumber || isLoading || !isDirty || !madeImageUpload){
-         setDisableSaveBtn(true)
-      }else{
-         setDisableSaveBtn(false)
-      }
+      errors.firstName || 
+      errors.birthday ||
+      !phoneNumber ||
+      isLoading || !isDirty || !madeImageUpload  ? setDisableSaveBtn(true) : setDisableSaveBtn(false)
+
 
       return () => {
          dispatch(setThereAreChanges(false))
